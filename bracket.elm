38751f9bda5cae868b18c0c437954dae2aa176bc
Michael Sondergaard <msond@cs.au.dk>
2013-10-28 16:40:29 +0100

    Cleanup dependencies

--- bracket.elm
+++ bracket.elm
@@ -1,10 +1,9 @@
 module Bracket where
-import Either (Either,Left,Right)
-import Color
 
 -- A 'brack' is a name of either a player or a team,
 -- associated with a score
-type Brack_desc = {score:Int, name:String}
+type Brack_desc = { score:Int,
+                    name:String }
 
 brackName : Brack -> String
 brackName b = case b of
@@ -26,37 +25,34 @@ type Match = { top:Brack, bottom:Brack }
 data Bracket a = Leaf a
                | InnerNode a (Bracket a) (Bracket a)
 
-renderBracket : Bracket (Either Match Brack)   ->
-                ((Either Match Brack) -> (Int, Int, Form)) ->
-                Form
--- Above type is intentionally specific to avoid unification bug
+renderBracket : Bracket a ->
+                (a -> (Int, Int, Form)) ->
+                (Int, Int, Form)
 renderBracket b draw =
-        let render : Bracket (Either Match Brack) -> (Int, Int, Form)
-            render b =
+        let render b =
               case b of
                 Leaf match ->
                   let (w,h,drawn) = draw match in
                   (w,h,drawn)
                 InnerNode match bl br ->
-                  let (_,h1,left)  = render bl
-                      (_,h2,right) = render br
-                      (w,_, drawn) = draw match
+                  let (w1,h1,left)  = render bl
+                      (w2,h2,right) = render br
+                      (w,h, drawn) = draw match
                   in
-                  (w,
-                  h1 + h2 + 30,
+                  (w+w1,
+                  h1+h2,
                   group
                      [drawn,
                       left  |> move (toFloat (-w - 50), toFloat -h1),
                       right |> move (toFloat (-w - 50), toFloat  h2),
-                      (traced (dashed Color.black)
+                      (traced (dashed black)
                               (path [(toFloat -w/2 - 50, toFloat -h1),
                                      (toFloat -w/2 - 25, toFloat -h1),
                                      (toFloat -w/2 - 25,0)])),
-                      (traced (dashed Color.black)
+                      (traced (dashed black)
                               (path [(toFloat -w/2 - 50, toFloat h2),
                                      (toFloat -w/2 - 25, toFloat h2),
                                      (toFloat -w/2 - 25,0),
                                      (toFloat -w/2,0)]))
                       ])
-            (_,_,r) = render b
-        in r
+        in render b
