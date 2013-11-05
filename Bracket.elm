5999e8c7932593323d6ba9653f06e9bebb663ddf
Michael Sondergaard <msond@cs.au.dk>
2013-11-05 20:03:00 +0100

    More graphics and remove backtype

--- Bracket.elm
+++ Bracket.elm
@@ -21,13 +21,11 @@ anyMatch fun m = case m of
                   Two b1 b2 -> fun b1 || fun b2
 
 -- Ctors for common types
-player name = Brack 0 name False Player
-team name   = Brack 0 name False Team
-teamWithScore name score   = Brack score name False Team
-playerWithScore name score = Brack score name False Player
+player name = Brack 0 name False
+playerWithScore name score = Brack score name False
 
 -- Two brackets are deemed equal if their type and names are equal
-brackEq b1 b2 = .name b1 == .name b2 && .typ b1 == .typ b2
+brackEq b1 b2 = .name b1 == .name b2
 
 matchEq : Match -> Match -> Bool
 matchEq m1 m2 = case (m1,m2) of
@@ -46,6 +44,9 @@ maxScore m = case m of
 -- Tree bracket structure
 data Bracket = Leaf Match
              | InnerNode Match Bracket Bracket
+
+match : Bracket -> Match
+match b = case b of {Leaf m -> m; InnerNode m _ _ -> m}
 
 finished : Bracket -> Bool
 finished b = anyBracket
@@ -67,6 +68,12 @@ anyBracket fun b =
      Leaf a -> (fun a)
      InnerNode a b1 b2 -> (fun a) || anyBracket fun b1 || anyBracket fun b2
 
+gayPurple = solid (rgb 79 54 153)
+lineStyle = { gayPurple | width <- 3,
+                          cap <- Round,
+                          join <- Sharp 0.1}
+
+
 renderBracket : Bracket -> (Match -> (Int, Int, Form)) -> (Int, Int, [Form])
 renderBracket b draw =
   case b of
@@ -74,20 +81,16 @@ renderBracket b draw =
       let (w,h,drawn) = draw match in
       (w,h,[drawn])
     InnerNode match bl br ->
-      let barrstyle      = if finished br then dashed black
-                                          else dashed white
-          tarrstyle      = if finished bl then dashed black
-                                          else dashed white
-          (w1,h1,left)  = renderBracket bl draw
+      let (w1,h1,left)  = renderBracket bl draw
           (w2,h2,right) = renderBracket br draw
           (w,h, drawn)  = draw match
           left'         = map (move (toFloat (-w - 50), toFloat -h1)) left
           right'        = map (move (toFloat (-w - 50), toFloat h2))  right
-          tarrw         = traced tarrstyle
+          tarrw         = traced lineStyle
                            (path [(toFloat -w/2 - 50, toFloat -h1),
                                   (toFloat -w/2 - 25, toFloat -h1),
                                   (toFloat -w/2 - 25,0)])
-          barrw         = traced barrstyle
+          barrw         = traced lineStyle
                            (path [(toFloat -w/2 - 50, toFloat h2),
                                   (toFloat -w/2  - 25, toFloat h2),
                                   (toFloat -w/2  - 25,0),
